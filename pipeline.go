45f2949f9b45183a3f7e4a864dbe04eda5711701
Vitor De Mario <vitor.demario@mendelics.com.br>
2015-02-10 18:37:42 -0200

    Revert "Interrupt operation"
    This reverts commit ef5c710838e4c5fa94c6c0f7d5490d043f999c9d.
    
    Interrupt is much more complicated than this simple attempt makes it look like. Will need
    a much more thorough suite of tests.

--- pipeline.go
+++ pipeline.go
@@ -3,12 +3,11 @@ package garoa
 import "log"
 
 type step struct {
-	degree    int
-	input     chan interface{}
-	output    chan interface{}
-	interrupt chan signal
-	action    PipelineFunc
-	done      chan signal
+	degree int
+	input  chan interface{}
+	output chan interface{}
+	action PipelineFunc
+	done   chan signal
 }
 
 // Pipeline represents a series of steps, interconnected by channels. Values will be passed through channels
@@ -23,7 +22,6 @@ type Pipeline struct {
 	steps     []step
 	done      chan signal
 	stepsDone chan signal
-	interrupt chan signal
 }
 
 // Run starts the pipeline. All the steps start communicating and the values from the input flow through
@@ -33,31 +31,19 @@ type Pipeline struct {
 // and all steps have finished processing. Notice it is still possible for the final output channel
 // to be fully filled which will cause the Pipeline to block. Values are not discarded after the last step
 // runs, they must be treated.
-func (pipeline *Pipeline) Run() chan signal {
+func (pipeline Pipeline) Run() chan signal {
 	numSteps := len(pipeline.steps)
 
 	pipeline.done = make(chan signal)
 	pipeline.stepsDone = make(chan signal, numSteps)
-	pipeline.interrupt = make(chan signal)
 
 	for i := 0; i < numSteps; i++ {
-		pipeline.steps[i].interrupt = make(chan signal)
 		pipeline.runStep(pipeline.steps[i])
 	}
 
 	go func() {
-		<-pipeline.interrupt            // wait for an interrupt to the whole pipeline
-		for i := 0; i < numSteps; i++ { // if it comes, tell all steps to stop
-			step := pipeline.steps[i]
-			for j := 0; j < step.degree; j++ {
-				pipeline.steps[i].interrupt <- signal{}
-			}
-		}
-	}()
-
-	go func() {
 		acked := 0
-		for _ = range pipeline.stepsDone { // wait for the done signal from each step, should come even on interrupt
+		for _ = range pipeline.stepsDone {
 			acked++
 			if acked == numSteps {
 				close(pipeline.stepsDone)
@@ -69,34 +55,20 @@ func (pipeline *Pipeline) Run() chan signal {
 	return pipeline.done
 }
 
-func (pipeline *Pipeline) Interrupt() {
-	pipeline.interrupt <- signal{}
-}
-
-func (pipeline *Pipeline) runStep(step step) {
+func (pipeline Pipeline) runStep(step step) {
 
 	step.done = make(chan signal, step.degree)
 
 	for i := 0; i < step.degree; i++ {
 		go func() {
-		loop:
-			for {
-				select {
-				case <-step.interrupt:
-					// stop consuming step.input channel, break out of the loop
-					break loop
-				case value, ok := <-step.input:
-					if !ok { // channel closed
-						break loop
-					}
-					v, err := step.action(value)
-					if err == nil && step.output != nil && v != nil { // there are no errors, the return value is non-nil and there is a valid output channel
-						step.output <- v
-					} else if err != nil {
-						log.Printf("Error: '%v' applying action to %v\n", err, value)
-					} else {
-						// silently discard, either there is no output channel or a nil value has been received from the action
-					}
+			for value := range step.input {
+				v, err := step.action(value)
+				if err == nil && step.output != nil && v != nil { // there are no errors, the return value is non-nil and there is a valid output channel
+					step.output <- v
+				} else if err != nil {
+					log.Printf("Error: '%v' applying action to %v\n", err, value)
+				} else {
+					// silently discard, either there is no output channel or a nil value has been received from the action
 				}
 			}
 			step.done <- signal{}
